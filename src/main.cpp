384d5952d152a596c5ee878a90f0ccc6791760d9
Christian Speckner <christian.speckner@mayflower.de>
2020-05-05 20:36:28 +0200

    Checkpoint.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-extern "C" void MyAssertFailed (const char* assertion, const char* file, unsigned int line)
+extern "C"
+void MyAssertFailed (const char* assertion, const char* file, unsigned int line)
 {
     cerr << "ASSERTION FAILED: " << assertion << " in " << file << ":" << line << endl;
 
