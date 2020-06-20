b3586cdc2d50296e7dd2cbf958f28e66cfdf3ccb
Christian Speckner <christian.speckner@mayflower.de>
2020-06-20 14:55:09 +0200

    Cleanup.

--- src/EmPalmOS.h
+++ src/EmPalmOS.h
@@ -27,70 +27,12 @@ class EmPalmOS {
     static void Load(SessionFile&);
     static void Dispose(void);
 
-#if 0  // CSDUBIOUS
-   public:
-    static void CheckStackPointerAssignment(void);
-    static void CheckStackPointerDecrement(void);
-    static void CheckKernelStack(void);
-    static void RememberStackChunk(const EmPalmChunk&);
-    static void RememberBootStack(void);
-    static void RememberKernelStack(void);
-    static void RememberStackRange(const StackRange&);
-    static void SetCurrentStack(const StackRange&);
-    static void ForgetStack(emuptr);
-    static void ForgetStacksIn(emuptr, uint32);
-    static StackRange GetBootStack(void);
-    static Bool IsInStack(emuptr);
-    static Bool IsInStackBlock(emuptr);
-    static void GenerateStackCrawl(EmStackFrameList& frameList);
-    static void PrefsChanged(PrefKeyType, void*);
-
-#endif
-
    protected:
     static Bool HandleTrap15(ExceptionNumber);
     static Bool HandleJSR_Ind(emuptr oldPC, emuptr dest);
 
-#if 0  // CSDUBIOUS
-    static void HandleNewPC(emuptr dest);
-    static void HandleNewSP(EmStackChangeType);
-#endif
-
    private:
     static Bool HandleSystemCall(Bool fromTrap);
 };
-
-#if 0  // CSDUBIOUS
-
-struct StackRange {
-    StackRange() : fTop(0), fLowWaterMark(0), fBottom(0) {}
-    StackRange(const StackRange& other)
-        : fTop(other.fTop), fLowWaterMark(other.fLowWaterMark), fBottom(other.fBottom) {}
-    StackRange(emuptr bottom, emuptr top) : fTop(top), fLowWaterMark(top), fBottom(bottom) {}
-
-    bool operator==(const StackRange& other) const { return fBottom == other.fBottom; }
-
-    bool operator!=(const StackRange& other) const { return fBottom != other.fBottom; }
-
-    bool operator<(const StackRange& other) const { return fBottom < other.fBottom; }
-
-    bool operator>(const StackRange& other) const { return fBottom > other.fBottom; }
-
-    // (Order these this way so that they look correct in a debugger)
-
-    emuptr fTop;
-    emuptr fLowWaterMark;
-    emuptr fBottom;
-};
-
-typedef vector<StackRange> StackList;
-
-class EmStream;
-EmStream& operator<<(EmStream&, const StackRange&);
-EmStream& operator>>(EmStream&, StackRange&);
-
-class EmPalmChunk;
-
-#endif
 
 #endif /* EmPalmOS_h */
