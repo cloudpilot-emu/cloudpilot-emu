366173b3f1ac89550d833f10e70c0835fe4eda1f
Christian Speckner <christian.speckner@mayflower.de>
2021-02-19 16:46:44 +0100

    Draw silkscreen.

--- cloudpilot-ionic/src/app/page/emulation/emulation.page.ts
+++ cloudpilot-ionic/src/app/page/emulation/emulation.page.ts
@@ -3,6 +3,7 @@ import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angula
 import { EmulationService } from './../../service/emulation.service';
 
 const SCALE = 3;
+const SILKSCREEN_URL = 'assets/silkscreen.png';
 @Component({
     selector: 'app-emulation',
     templateUrl: './emulation.page.html',
@@ -14,11 +15,14 @@ export class EmulationPage implements AfterViewInit {
     ngAfterViewInit(): void {
         // tslint:disable-next-line: no-non-null-assertion
         const ctx = this.canvasRef.nativeElement.getContext('2d')!;
-        ctx.imageSmoothingEnabled = false;
 
         ctx.fillStyle = '#aaa';
         ctx.rect(0, 0, this.canvasWidth, this.canvasHeight);
         ctx.fill();
+
+        ctx.imageSmoothingEnabled = false;
+
+        this.drawSilkscreen();
     }
 
     get canvasWidth(): number {
@@ -45,6 +49,27 @@ export class EmulationPage implements AfterViewInit {
         // tslint:disable-next-line: no-non-null-assertion
         this.canvasRef.nativeElement.getContext('2d')!.drawImage(canvas, 0, 0, SCALE * 160, SCALE * 160);
     };
+
+    private async drawSilkscreen(): Promise<void> {
+        const image = new Image();
+
+        await new Promise<void>((resolve, reject) => {
+            image.onload = () => resolve();
+            image.onerror = () => reject();
+
+            image.src = SILKSCREEN_URL;
+        });
+
+        // tslint:disable-next-line: no-non-null-assertion
+        const ctx = this.canvasRef.nativeElement.getContext('2d')!;
+
+        ctx.imageSmoothingEnabled = true;
+        ctx.imageSmoothingQuality = 'high';
+
+        ctx.drawImage(image, 0, 160 * SCALE, 160 * SCALE, 60 * SCALE);
+
+        ctx.imageSmoothingEnabled = false;
+    }
 
     @ViewChild('canvas') private canvasRef!: ElementRef<HTMLCanvasElement>;
 }
