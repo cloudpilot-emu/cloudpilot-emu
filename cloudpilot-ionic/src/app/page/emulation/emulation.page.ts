d9aad487a7297f141dfccbcbbb0ba9d8b492a51c
Christian Speckner <christian.speckner@mayflower.de>
2021-02-19 11:55:15 +0100

    Hello canvas.

--- cloudpilot-ionic/src/app/page/emulation/emulation.page.ts
+++ cloudpilot-ionic/src/app/page/emulation/emulation.page.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-emulation',
     templateUrl: './emulation.page.html',
     styleUrls: ['./emulation.page.scss'],
 })
-export class EmulationPage implements OnInit {
+export class EmulationPage implements AfterViewInit {
     constructor() {}
 
-    ngOnInit() {}
+    ngAfterViewInit(): void {
+        // tslint:disable-next-line: no-non-null-assertion
+        const ctx = this.canvasRef.nativeElement.getContext('2d')!;
+
+        ctx.fillStyle = 'green';
+        ctx.rect(0, 0, this.canvasWidth, this.canvasHeight);
+        ctx.fill();
+    }
+
+    private get canvasWidth(): number {
+        return 3 * 160;
+    }
+
+    private get canvasHeight(): number {
+        return 3 * 250;
+    }
+
+    @ViewChild('canvas') private canvasRef!: ElementRef<HTMLCanvasElement>;
 }
