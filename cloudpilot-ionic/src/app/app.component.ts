1320f8aa3ce5326ddd4646295cf1af98cef9317a
Christian Speckner <christian.speckner@mayflower.de>
2021-02-19 10:50:43 +0100

    Kill dead code.

--- cloudpilot-ionic/src/app/app.component.ts
+++ cloudpilot-ionic/src/app/app.component.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-    constructor(private platform: Platform, private splashScreen: SplashScreen, private statusBar: StatusBar) {
-        this.initializeApp();
-    }
-
-    initializeApp() {}
-}
+export class AppComponent {}
--- cloudpilot-ionic/src/app/app.module.ts
+++ cloudpilot-ionic/src/app/app.module.ts
@@ -6,15 +6,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { PageLockService } from './service/page-lock.service';
 import { RouteReuseStrategy } from '@angular/router';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { StorageService } from './service/storage.service';
 
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
     imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-    providers: [StatusBar, SplashScreen, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
     bootstrap: [AppComponent],
 })
 export class AppModule {
