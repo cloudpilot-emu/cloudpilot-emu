51bd05aa92fdb4ce28c4c4f425abaf5f78c7220a
Christian Speckner <christian.speckner@mayflower.de>
2021-03-05 11:29:21 +0100

    Service worker.

--- cloudpilot-ionic/src/app/app.module.ts
+++ cloudpilot-ionic/src/app/app.module.ts
@@ -7,12 +7,19 @@ import { EmulationService } from './service/emulation.service';
 import { NgModule } from '@angular/core';
 import { PageLockService } from './service/page-lock.service';
 import { RouteReuseStrategy } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { StorageService } from './service/storage.service';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        AppRoutingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ],
     providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
     bootstrap: [AppComponent],
 })
