f5286c8cfcc7029d898c9c73a73e8a4ee17e1265
Christian Speckner <christian.speckner@mayflower.de>
2021-03-07 21:54:30 +0100

    Styling, save a bit screen real estate by scaling the tab bar on the emulation page.

--- cloudpilot-ionic/src/app/tabs/tabs.page.ts
+++ cloudpilot-ionic/src/app/tabs/tabs.page.ts
@@ -1,4 +1,25 @@
+import { HEIGHT, SCALE, WIDTH } from './../page/emulation/helper/CanvasHelper';
+
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+function canvasArea(availableHeight: number, availableWidth: number): number {
+    availableHeight = Math.min(availableHeight, HEIGHT / devicePixelRatio);
+    availableWidth = Math.min(availableWidth, WIDTH / devicePixelRatio);
+
+    let scaledWidth: number;
+    let scaledHeight: number;
+
+    if (availableWidth / availableHeight > WIDTH / HEIGHT) {
+        scaledHeight = availableHeight;
+        scaledWidth = (availableHeight / HEIGHT) * WIDTH;
+    } else {
+        scaledWidth = availableWidth;
+        scaledHeight = (availableWidth / WIDTH) * HEIGHT;
+    }
+
+    return scaledWidth * scaledHeight;
+}
 
 @Component({
     selector: 'app-tabs',
@@ -6,5 +27,21 @@ import { Component } from '@angular/core';
     styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
-    constructor() {}
+    constructor(private router: Router) {
+        window.addEventListener('resize', this.updateUseSmallUI);
+
+        this.updateUseSmallUI();
+    }
+
+    get smallUI(): boolean {
+        return this.router.url === '/tab/emulation' && this.useSmallUI;
+    }
+
+    private updateUseSmallUI = (): void => {
+        this.useSmallUI =
+            canvasArea(window.innerHeight - 44 - 50 - 6, window.innerWidth - 6) <
+            canvasArea(window.innerHeight - 44 - 25 - 6, window.innerWidth - 6);
+    };
+
+    private useSmallUI = false;
 }
