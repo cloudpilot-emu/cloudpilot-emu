ea7b78d7c9a20bbcc8924636a930734d78dbe73f
Christian Speckner <christian.speckner@mayflower.de>
2021-03-13 00:46:53 +0100

    Add missing import.

--- cloudpilot-ionic/src/app/component/shared-component.module.ts
+++ cloudpilot-ionic/src/app/component/shared-component.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { HelpComponent } from './help/help.component';
+import { IonicModule } from '@ionic/angular';
 import { MarkdownModule } from 'ngx-markdown';
 import { NgModule } from '@angular/core';
 
 @NgModule({
     declarations: [HelpComponent],
-    imports: [CommonModule, MarkdownModule.forChild()],
+    imports: [CommonModule, MarkdownModule.forChild(), IonicModule],
 })
 export class SharedComponentModule {}
