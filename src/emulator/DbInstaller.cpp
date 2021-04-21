c1a6dd622737b61cbda2b4c9766280c3ef4351dc
Christian Speckner <christian.speckner@mayflower.de>
2021-04-21 23:19:04 +0200

    Error if deleting a DB fails.

--- src/emulator/DbInstaller.cpp
+++ src/emulator/DbInstaller.cpp
@@ -10,6 +10,7 @@ DbInstaller::Result DbInstaller::Install(size_t bufferSize, uint8* buffer) {
     if (EmHAL::GetAsleep()) return Result::failure;
 
     size_t bytesRead = 0;
+    bool failedToOverwrite = false;
 
     CallbackWrapper readProcP([&]() {
         CALLED_SETUP_STDARG("Err", "void* dataP, UInt32* sizeP, void* userDataP");
@@ -32,7 +33,7 @@ DbInstaller::Result DbInstaller::Install(size_t bufferSize, uint8* buffer) {
         PUT_RESULT_VAL(Err, 0);
     });
 
-    CallbackWrapper deleteProcP([]() {
+    CallbackWrapper deleteProcP([&]() {
         CALLED_SETUP_STDARG(
             "Boolean",
             "const char* nameP, UInt16 version, UInt16 cardNo, LocalID dbID, void* userDataP");
@@ -40,9 +41,15 @@ DbInstaller::Result DbInstaller::Install(size_t bufferSize, uint8* buffer) {
         CALLED_GET_PARAM_VAL(UInt16, cardNo);
         CALLED_GET_PARAM_VAL(LocalID, dbID);
 
+        UInt16 attributes;
+
+        DmDatabaseInfo(cardNo, dbID, NULL, &attributes, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+                       NULL, NULL);
+
         if (DmDeleteDatabase(cardNo, dbID) == 0) {
             PUT_RESULT_VAL(Boolean, 1);
         } else {
+            failedToOverwrite = true;
             PUT_RESULT_VAL(Boolean, 0);
         }
     });
@@ -50,7 +57,8 @@ DbInstaller::Result DbInstaller::Install(size_t bufferSize, uint8* buffer) {
     LocalID localId;
     Boolean needsReset = false;
 
-    if (ExgDBRead(readProcP, deleteProcP, 0, &localId, 0, &needsReset, true) == 0)
+    if (ExgDBRead(readProcP, deleteProcP, 0, &localId, 0, &needsReset, true) == 0 &&
+        !failedToOverwrite)
         return needsReset ? Result::needsReboot : Result::success;
 
     return Result::failure;
