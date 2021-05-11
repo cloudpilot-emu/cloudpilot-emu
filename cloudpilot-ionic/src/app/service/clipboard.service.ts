0af25c543417d6e80a98aa6c4ac4d52b46398d99
Christian Speckner <christian.speckner@mayflower.de>
2021-05-11 21:59:45 +0200

    Rework clipboard handling, don't rely on browser sniffing.

--- cloudpilot-ionic/src/app/service/clipboard.service.ts
+++ cloudpilot-ionic/src/app/service/clipboard.service.ts
@@ -1,16 +1,11 @@
-import {
-    Cloudpilot,
-    SuspendContextClipboardCopy,
-    SuspendContextClipboardPaste,
-    SuspendKind,
-} from '../helper/Cloudpilot';
+import { Cloudpilot, SuspendContextClipboardPaste, SuspendKind } from '../helper/Cloudpilot';
 
 import { AlertController } from '@ionic/angular';
 import { AlertService } from 'src/app/service/alert.service';
 import { Injectable } from '@angular/core';
-import { isSafari } from './../helper/browser';
 
 const READ_CLIPBOARD_TTL = 3000;
+const E_PERMISSION_DENIED = new Error('permission denied');
 
 @Injectable({
     providedIn: 'root',
@@ -19,7 +14,7 @@ export class ClipboardService {
     constructor(private alertController: AlertController, private alertService: AlertService) {}
 
     isSupported(): boolean {
-        return !!navigator.clipboard?.readText;
+        return !!navigator.clipboard?.readText && !!navigator.clipboard?.writeText;
     }
 
     handleSuspend(cloudpilot: Cloudpilot): void {
@@ -38,84 +33,169 @@ export class ClipboardService {
 
     private async handleCopy(cloudpilot: Cloudpilot): Promise<void> {
         const ctx = cloudpilot.getSuspendContextClipboardCopy();
+        const clipboardContent = ctx.GetClipboardContent();
 
-        if (isSafari) {
-            await this.handleCopySafari(ctx);
-        } else {
-            try {
-                await navigator.clipboard.writeText(ctx.GetClipboardContent());
+        try {
+            await this.tryCopyDirect(clipboardContent);
 
-                ctx.Resume();
-            } catch (e) {
-                await this.alertService.errorMessage('Copy to host clipboard failed.');
+            ctx.Resume();
 
+            return;
+        } catch (e) {
+            if (e === E_PERMISSION_DENIED) {
                 ctx.Cancel();
+
+                return this.msgPermissionDenied();
             }
+        }
+
+        try {
+            await this.tryCopyWithInteraction(clipboardContent);
+
+            ctx.Resume();
+        } catch (e) {
+            await this.msgCopyFailed();
+
+            ctx.Cancel();
         }
     }
 
-    private async handleCopySafari(ctx: SuspendContextClipboardCopy) {
-        const alert = await this.alertController.create({
-            header: 'Confirm Copy',
-            message: 'Please confirm that you want to copy to the host clipboard',
-            backdropDismiss: false,
-            buttons: [
-                {
-                    text: 'Copy',
-                    handler: async () => {
-                        try {
-                            await navigator.clipboard.writeText(ctx.GetClipboardContent());
+    private async tryCopyDirect(clipboardText: string): Promise<void> {
+        try {
+            await navigator.clipboard.writeText(clipboardText);
+        } catch (e) {
+            if (await this.isPermissionDenied('clipboard-write' as PermissionName)) throw E_PERMISSION_DENIED;
 
-                            alert.dismiss();
+            throw e;
+        }
+    }
 
-                            ctx.Resume();
-                        } catch (e) {
-                            alert.dismiss();
+    private tryCopyWithInteraction(clipboardText: string): Promise<boolean> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const alert = await this.alertController.create({
+                    header: 'Confirm Copy',
+                    message: 'Please confirm that you want to copy to the host clipboard',
+                    backdropDismiss: false,
+                    buttons: [
+                        {
+                            text: 'Copy',
+                            handler: async () => {
+                                alert.dismiss();
 
-                            await this.alertService.errorMessage('Copy to host clipboard failed.');
+                                try {
+                                    await navigator.clipboard.writeText(clipboardText);
 
-                            ctx.Cancel();
-                        }
-                    },
-                },
-                {
-                    text: 'Cancel',
-                    handler: () => {
-                        alert.dismiss();
-                        ctx.Cancel();
-                    },
-                },
-            ],
+                                    resolve(true);
+                                } catch (e) {
+                                    reject(e);
+                                }
+                            },
+                        },
+                        {
+                            text: 'Cancel',
+                            handler: () => {
+                                alert.dismiss();
+
+                                resolve(false);
+                            },
+                        },
+                    ],
+                });
+
+                alert.present();
+            } catch (e) {
+                reject(e);
+            }
         });
-
-        await alert.present();
     }
 
     private async handlePaste(cloudpilot: Cloudpilot): Promise<void> {
+        const clipboardContent = await this.tryPaste();
+
         const ctx = cloudpilot.getSuspendContextClipboardPaste();
+        ctx.Resume(clipboardContent);
 
+        this.clipboardContent = clipboardContent;
+        this.lastClipboardReadAt = performance.now();
+    }
+
+    private async tryPaste(): Promise<string> {
         if (performance.now() - this.lastClipboardReadAt < READ_CLIPBOARD_TTL) {
-            ctx.Resume(this.clipboardContent);
-        } else if (isSafari) {
-            await this.handlePasteSafari(ctx);
-        } else {
-            try {
-                this.clipboardContent = await navigator.clipboard.readText();
-                this.lastClipboardReadAt = performance.now();
+            return this.clipboardContent;
+        }
 
-                ctx.Resume(this.clipboardContent);
-            } catch (e) {
-                await this.alertService.errorMessage('Paste from host clipboard failed.');
+        try {
+            return await this.tryPasteDirect();
+        } catch (e) {
+            if (e === E_PERMISSION_DENIED) {
+                await this.msgPermissionDenied();
 
-                ctx.Cancel();
+                return '';
             }
         }
+
+        try {
+            return await this.tryPasteWithInteraction();
+        } catch (e) {
+            await this.msgPasteFailed();
+
+            return '';
+        }
+    }
+
+    private async tryPasteDirect(): Promise<string> {
+        try {
+            return await navigator.clipboard.readText();
+        } catch (e) {
+            if (await this.isPermissionDenied('clipboard-read' as PermissionName)) throw E_PERMISSION_DENIED;
+
+            throw e;
+        }
+    }
+
+    private tryPasteWithInteraction(): Promise<string> {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const alert = await this.alertController.create({
+                    header: 'Confirm Paste',
+                    message: 'Please confirm that you want to paste from the host clipboard',
+                    backdropDismiss: false,
+                    buttons: [
+                        {
+                            text: 'Paste',
+                            handler: async () => {
+                                alert.dismiss();
+
+                                try {
+                                    resolve(await navigator.clipboard.readText());
+                                } catch (e) {
+                                    reject(e);
+                                }
+                            },
+                        },
+                        {
+                            text: 'Cancel',
+                            handler: () => {
+                                alert.dismiss();
+
+                                resolve('');
+                            },
+                        },
+                    ],
+                });
+
+                alert.present();
+            } catch (e) {
+                reject(e);
+            }
+        });
     }
 
     private async handlePasteSafari(ctx: SuspendContextClipboardPaste): Promise<void> {
         const alert = await this.alertController.create({
             header: 'Confirm Paste',
-            message: 'Please confirm that you want to paste from the host clipboard',
+            message: 'Please confirm that you want to paste from the host clipboard.',
             backdropDismiss: false,
             buttons: [
                 {
@@ -149,6 +229,35 @@ export class ClipboardService {
         });
 
         await alert.present();
+    }
+
+    private async isPermissionDenied(name: PermissionName): Promise<boolean> {
+        if (navigator.permissions) {
+            try {
+                const status = await navigator.permissions.query({ name });
+
+                return status?.state === 'denied';
+            } catch (e) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    private msgPermissionDenied(): Promise<void> {
+        return this.alertService.errorMessage(`
+        Cloudpilot has no permission to access the clipboard. Please check your browser's settings
+        and grant the necessary permission to ${location.origin} .
+    `);
+    }
+
+    private msgCopyFailed(): Promise<void> {
+        return this.alertService.errorMessage('Failed to copy to the host clipboard.');
+    }
+
+    private msgPasteFailed(): Promise<void> {
+        return this.alertService.errorMessage('Failed to paste from the host clipboard.');
     }
 
     private clipboardContent = '';
