a5dca7c6770a51c321763c912a2084a753e015a3
Christian Speckner <christian.speckner@mayflower.de>
2021-06-27 16:57:13 +0200

    Dependency update.

--- cloudpilot-ionic/src/app/helper/version.ts
+++ cloudpilot-ionic/src/app/helper/version.ts
@@ -1,7 +1,6 @@
-import * as pkg from '../../../package.json';
-
 import { REVISION } from '../../revision';
 import { environment } from './../../environments/environment';
+import pkg from '../../../package.json';
 
 export const VERSION = environment.releaseExtra
     ? `${pkg.version}-${REVISION} (${environment.releaseExtra})`
