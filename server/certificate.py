741f12041c09d2de28d6d75f99380d8c198ba150
Christian Speckner <christian.speckner@mayflower.de>
2021-07-06 22:37:43 +0200

    Small fix for android.

--- server/certificate.py
+++ server/certificate.py
@@ -10,7 +10,7 @@ CN_DEFAULT = "cloudpilot-server"
 VALIDITY_YEARS = 1
 BASIC_CONSTRAINTS = b'CA:TRUE,pathlen:0'
 BASIC_KEY_USAGE = b'serverAuth'
-EXTENDED_KEY_USAGE = b'keyEncipherment,keyAgreement,cRLSign'
+EXTENDED_KEY_USAGE = b'keyEncipherment,keyAgreement,cRLSign,digitalSignature'
 
 REGEX_IP = re.compile('^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$')
 REGEX_NAME = re.compile('^[a-zA-Z\d\.\-]+$')
