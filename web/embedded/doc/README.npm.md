0e0bae7e213425ab83d47382dd4c4ffbdf4fd025
Christian Speckner <christian.speckner@mayflower.de>
2022-07-25 21:49:39 +0200

    Generate NPM dist dir.

---
+++ web/embedded/doc/README.npm.md
@@ -0,0 +1,14 @@
+This is the embedded version of the CloudpilotEmu PalmOS emulator. It allows you
+to integrate virtual PalmOS devices into you web pages.
+
+Please see the documentation and the github page for more details:
+
+* [CloudpilotEmu on Github](https://github.com/cloudpilot-emu/cloudpilot-emu)
+* [CloudpilotEmu embedded documentation](https://cloudpilot-emu.github.io/embedded/)
+
+# License
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
--- web/script/dist-embedded.js
+++ web/script/dist-embedded.js
@@ -1,6 +1,21 @@
 const shelljs = require('shelljs');
 const path = require('path');
 const fs = require('fs');
+const packageJson = require('../package.json');
+
+const PACKAGE_JSON_TEMPLATE = {
+    name: 'cloudpilot-emu-embedded',
+    author: 'Christian Speckner <cnspeckn@googlemail.com>',
+    license: 'GPL-3.0-or-later',
+    homepage: 'https://cloudpilot-emu.github.io/',
+    description: 'Embedded version of the CloudpilotEmu PalmOS emulator',
+    keywords: ['cloudpilot', 'cloudpilotemu', 'palmos', 'palm', 'emulator'],
+    files: ['README.md', 'LICENSE', 'dist/**/*'],
+    main: 'dist/cloudpilot-emu.js',
+    types: 'dist/cloudpilot-emu.d.ts',
+    dependencies: {},
+    scripts: {},
+};
 
 const OUT_DIR = path.normalize(path.join(__dirname, '..', 'dist-embedded'));
 const BUILD_SRC = path.normalize(path.join(__dirname, '..', 'build-embedded'));
@@ -9,9 +24,31 @@ const TYPEDOC_SRC = path.normalize(path.join(__dirname, '..', 'docs-embedded'));
 const TYPEDOC_DEST = path.normalize(path.join(OUT_DIR, 'reference'));
 const README = path.normalize(path.join(__dirname, '..', 'embedded', 'doc', 'README.md'));
 
+const NPM_PACKAGE_DIR = path.normalize(path.join(__dirname, '..', 'npm-embedded'));
+const NPM_DIST = ['cloudpilot-emu.js', 'cloudpilot-emu.d.ts', 'cloudpilot-emu.js.map', 'cloudpilot_web.wasm'].map((x) =>
+    path.normalize(path.join(BUILD_SRC, x))
+);
+const NPM_DIST_TARGET = path.normalize(path.join(NPM_PACKAGE_DIR, 'dist'));
+
+const LICENSE = path.normalize(path.join(__dirname, '..', '..', 'LICENSE'));
+const README_NPM_SOURCE = path.normalize(path.join(__dirname, '..', 'embedded', 'doc', 'README.npm.md'));
+const README_NPM_DEST = path.normalize(path.join(NPM_PACKAGE_DIR, 'README.md'));
+const PACKAGE_JSON_DEST = path.normalize(path.join(NPM_PACKAGE_DIR, 'package.json'));
+
 if (fs.existsSync(OUT_DIR)) shelljs.rm('-r', OUT_DIR);
 
 shelljs.mkdir(OUT_DIR);
 shelljs.cp('-R', BUILD_SRC, BUILD_DEST);
 shelljs.cp('-R', TYPEDOC_SRC, TYPEDOC_DEST);
 shelljs.cp(README, OUT_DIR);
+
+if (fs.existsSync(NPM_DIST_TARGET)) shelljs.rm('-r', NPM_DIST_TARGET);
+
+shelljs.mkdir('-p', NPM_DIST_TARGET);
+NPM_DIST.forEach((x) => shelljs.cp(x, NPM_DIST_TARGET));
+shelljs.cp(README_NPM_SOURCE, README_NPM_DEST);
+shelljs.cp(LICENSE, NPM_PACKAGE_DIR);
+fs.writeFileSync(
+    PACKAGE_JSON_DEST,
+    JSON.stringify({ ...PACKAGE_JSON_TEMPLATE, version: packageJson.version }, undefined, '  ')
+);
