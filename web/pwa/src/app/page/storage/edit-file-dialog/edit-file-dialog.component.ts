f2a631094af7850d75ce7dd9e26e39b6837a8a7d
Christian Speckner <christian.speckner@mayflower.de>
2023-01-09 21:42:14 +0100

    Fix build.

--- web/pwa/src/app/page/storage/edit-file-dialog/edit-file-dialog.component.ts
+++ web/pwa/src/app/page/storage/edit-file-dialog/edit-file-dialog.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-edit-file-dialog',
-  templateUrl: './edit-file-dialog.component.html',
-  styleUrls: ['./edit-file-dialog.component.css']
+    selector: 'app-edit-file-dialog',
+    templateUrl: './edit-file-dialog.component.html',
+    styleUrls: ['./edit-file-dialog.component.scss'],
 })
-export class EditFileDialogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class EditFileDialogComponent {
+    constructor() {}
 }
