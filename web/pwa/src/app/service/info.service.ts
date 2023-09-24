409156afe1be3495aad2d3465de95749c3885cff
Christian Speckner <christian.speckner@mayflower.de>
2023-09-24 16:24:25 +0200

    Fix bogus startup message.

--- web/pwa/src/app/service/info.service.ts
+++ web/pwa/src/app/service/info.service.ts
@@ -18,37 +18,7 @@ export class InfoService {
     }
 
     private async showInfo(infoId: number): Promise<number> {
-        if (infoId < 2 && infoId !== 1 && isIOS) {
-            await this.emulationService.bootstrapComplete();
-
-            await this.alertService.message(
-                'Offline mode on iOS 17',
-                `
-                iOS 17 ships with a browser bug that breaks the caching of CloudpilotEmu
-                for offline use. As a result, the app will currently not load and
-                work reliably on iOS 17 without an internet connection.
-                <br><br>
-                This is a browser bug that can only be fixed by Apple.
-            `,
-            );
-        }
-
-        if (infoId === 1 && isIOS) {
-            await this.emulationService.bootstrapComplete();
-
-            await this.alertService.message(
-                'Browser bug on iOS 17',
-                `
-                CloudpilotEmu now includes a workaround for the browser bug that prevents
-                reliable startup. However, running without an internet conection is
-                still broken and can only be fixed if Apple resolves the underlying bug.
-                <br><br>
-                If you disabled Service Workers you can now safely reenable them.
-            `,
-            );
-        }
-
-        if (infoId === 2 && isIOS) {
+        if (infoId === 2) {
             await this.emulationService.bootstrapComplete();
 
             await this.alertService.message(
