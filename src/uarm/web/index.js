372d57605655984f1c02b92e8991384b23656d48
Christian Speckner <christian.speckner@mayflower.de>
2023-12-15 15:57:22 +0100

    Work around a bad memory leak in Safari.

--- src/uarm/web/index.js
+++ src/uarm/web/index.js
@@ -2,150 +2,152 @@ import './setimmediate/setimmediate.js';
 import { Emulator } from './emulator.js';
 import { Database } from './database.js';
 
-const isIOSSafari = !navigator.userAgent.match(/(crios)|(fxios)/i);
-const isIOS =
-    !!navigator.platform.match(/iPhone|iPad|iPod/) ||
-    (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
-const isSafari = 'safari' in window || (isIOS && isIOSSafari);
+(function () {
+    const isIOSSafari = !navigator.userAgent.match(/(crios)|(fxios)/i);
+    const isIOS =
+        !!navigator.platform.match(/iPhone|iPad|iPod/) ||
+        (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
+    const isSafari = 'safari' in window || (isIOS && isIOSSafari);
 
-const labelNor = document.getElementById('nor-image');
-const labelNand = document.getElementById('nand-image');
-const labelSD = document.getElementById('sd-image');
-const speedDisplay = document.getElementById('speed');
-const logContainer = document.getElementById('log');
+    const labelNor = document.getElementById('nor-image');
+    const labelNand = document.getElementById('nand-image');
+    const labelSD = document.getElementById('sd-image');
+    const speedDisplay = document.getElementById('speed');
+    const logContainer = document.getElementById('log');
 
-const uploadNor = document.getElementById('upload-nor');
-const uploadNand = document.getElementById('upload-nand');
-const uploadSD = document.getElementById('upload-sd');
-const clearLog = document.getElementById('clear-log');
+    const uploadNor = document.getElementById('upload-nor');
+    const uploadNand = document.getElementById('upload-nand');
+    const uploadSD = document.getElementById('upload-sd');
+    const clearLog = document.getElementById('clear-log');
 
-const canvas = document.getElementsByTagName('canvas')[0];
-const canvasCtx = canvas.getContext('2d');
+    const canvas = document.getElementsByTagName('canvas')[0];
+    const canvasCtx = canvas.getContext('2d');
 
-let fileNor, fileNand, fileSd;
-let emulator;
+    let fileNor, fileNand, fileSd;
+    let emulator;
 
-function log(message) {
-    const line = document.createElement('div');
-    line.className = 'log-line';
-    line.innerText = message;
+    function log(message) {
+        const line = document.createElement('div');
+        line.className = 'log-line';
+        line.innerText = message;
 
-    logContainer.appendChild(line);
-    logContainer.scrollTop = logContainer.scrollHeight;
-}
-
-function updateLabels() {
-    labelNor.innerText = fileNor?.name ?? '[none]';
-    labelNand.innerText = fileNand?.name ?? '[none]';
-    labelSD.innerText = fileSd?.name ?? '[none]';
-}
-
-let fileInput;
-const openFile = () =>
-    new Promise((resolve, reject) => {
-        if (isSafari && fileInput) document.body.removeChild(fileInput);
-        fileInput = document.createElement('input');
-
-        fileInput.onchange = (evt) => {
-            console.log('onchange');
-
-            const target = evt.target;
-            const file = target?.files?.[0];
-
-            if (!file) reject(new Error('no files selected'));
-
-            const reader = new FileReader();
-
-            reader.onload = () =>
-                resolve({
-                    name: file.name,
-                    content: new Uint8Array(reader.result),
-                });
-            reader.onerror = () => reject(reader.error);
-
-            reader.readAsArrayBuffer(file);
-        };
-
-        fileInput.type = 'file';
-        fileInput.value = '';
-        fileInput.style.display = 'none';
-
-        if (isSafari) document.body.appendChild(fileInput);
-
-        fileInput.click();
-    });
-
-function clearCanvas() {
-    canvasCtx.fillStyle = '#fff';
-    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-const uploadHandler = (assign) => () =>
-    openFile()
-        .then((file) => {
-            assign(file);
-            updateLabels();
-
-            restart();
-        })
-        .catch((e) => console.error(e));
-
-async function restart() {
-    if (!(fileNor && fileNand)) return;
-
-    emulator?.destroy();
-    clearCanvas();
-
-    emulator = await Emulator.create(fileNor.content, fileNand.content, fileSd?.content, {
-        canvas: canvasCtx.canvas,
-        speedDisplay,
-        log,
-    });
-    emulator?.start();
-}
-
-async function main() {
-    const database = await Database.create();
-    clearCanvas();
-
-    fileNor = await database.getNor();
-    fileNand = await database.getNand();
-    fileSd = await database.getSd();
-
-    if (window.location.search.indexOf('?noload') < 0) {
-        log('Reload with ?noload appended to the URL if the emulator hangs on load due to invalid NOR or NAND');
-        log('---');
-
-        await restart();
+        logContainer.appendChild(line);
+        logContainer.scrollTop = logContainer.scrollHeight;
     }
 
-    updateLabels();
+    function updateLabels() {
+        labelNor.innerText = fileNor?.name ?? '[none]';
+        labelNand.innerText = fileNand?.name ?? '[none]';
+        labelSD.innerText = fileSd?.name ?? '[none]';
+    }
 
-    uploadNor.addEventListener(
-        'click',
-        uploadHandler((file) => {
-            database.putNor(file);
-            fileNor = file;
-        })
-    );
-    uploadNand.addEventListener(
-        'click',
-        uploadHandler((file) => {
-            database.putNand(file);
-            fileNand = file;
-        })
-    );
-    uploadSD.addEventListener(
-        'click',
-        uploadHandler((file) => {
-            database.putSd(file);
-            fileSd = file;
-        })
-    );
+    let fileInput;
+    const openFile = () =>
+        new Promise((resolve, reject) => {
+            if (isSafari && fileInput) document.body.removeChild(fileInput);
+            fileInput = document.createElement('input');
 
-    clearLog.addEventListener('click', () => (logContainer.innerHTML = ''));
+            fileInput.onchange = (evt) => {
+                console.log('onchange');
 
-    document.addEventListener('visibilitychange', () => (document.hidden ? emulator?.stop() : emulator?.start()));
-}
+                const target = evt.target;
+                const file = target?.files?.[0];
 
-main().catch((e) => console.error(e));
+                if (!file) reject(new Error('no files selected'));
+
+                const reader = new FileReader();
+
+                reader.onload = () =>
+                    resolve({
+                        name: file.name,
+                        content: new Uint8Array(reader.result),
+                    });
+                reader.onerror = () => reject(reader.error);
+
+                reader.readAsArrayBuffer(file);
+            };
+
+            fileInput.type = 'file';
+            fileInput.value = '';
+            fileInput.style.display = 'none';
+
+            if (isSafari) document.body.appendChild(fileInput);
+
+            fileInput.click();
+        });
+
+    function clearCanvas() {
+        canvasCtx.fillStyle = '#fff';
+        canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+    const uploadHandler = (assign) => () =>
+        openFile()
+            .then((file) => {
+                assign(file);
+                updateLabels();
+
+                restart();
+            })
+            .catch((e) => console.error(e));
+
+    async function restart() {
+        if (!(fileNor && fileNand)) return;
+
+        emulator?.destroy();
+        clearCanvas();
+
+        emulator = await Emulator.create(fileNor.content, fileNand.content, fileSd?.content, {
+            canvas: canvasCtx.canvas,
+            speedDisplay,
+            log,
+        });
+        emulator?.start();
+    }
+
+    async function main() {
+        const database = await Database.create();
+        clearCanvas();
+
+        fileNor = await database.getNor();
+        fileNand = await database.getNand();
+        fileSd = await database.getSd();
+
+        if (window.location.search.indexOf('?noload') < 0) {
+            log('Reload with ?noload appended to the URL if the emulator hangs on load due to invalid NOR or NAND');
+            log('---');
+
+            await restart();
+        }
+
+        updateLabels();
+
+        uploadNor.addEventListener(
+            'click',
+            uploadHandler((file) => {
+                database.putNor(file);
+                fileNor = file;
+            })
+        );
+        uploadNand.addEventListener(
+            'click',
+            uploadHandler((file) => {
+                database.putNand(file);
+                fileNand = file;
+            })
+        );
+        uploadSD.addEventListener(
+            'click',
+            uploadHandler((file) => {
+                database.putSd(file);
+                fileSd = file;
+            })
+        );
+
+        clearLog.addEventListener('click', () => (logContainer.innerHTML = ''));
+
+        document.addEventListener('visibilitychange', () => (document.hidden ? emulator?.stop() : emulator?.start()));
+    }
+
+    main().catch((e) => console.error(e));
+})();
--- src/uarm/web/worker.js
+++ src/uarm/web/worker.js
@@ -1,294 +1,297 @@
 importScripts('../src/uarm_web.js', './setimmediate/setimmediate.js');
 
-const messageQueue = [];
-let dispatchInProgress = false;
+(function () {
+    const messageQueue = [];
+    let dispatchInProgress = false;
 
-let emulator;
+    let emulator;
 
-let framePool = [];
+    let framePool = [];
 
-class Emulator {
-    constructor(module, { onSpeedDisplay, onFrame, log }) {
-        this.module = module;
-        this.onSpeedDisplay = onSpeedDisplay;
-        this.onFrame = onFrame;
-        this.log = log;
+    class Emulator {
+        constructor(module, { onSpeedDisplay, onFrame, log }) {
+            this.module = module;
+            this.onSpeedDisplay = onSpeedDisplay;
+            this.onFrame = onFrame;
+            this.log = log;
 
-        this.cycle = module.cwrap('cycle', undefined, ['number']);
-        this.getFrame = module.cwrap('getFrame', 'number', []);
-        this.resetFrame = module.cwrap('resetFrame', undefined, []);
-        this.currentIps = module.cwrap('currentIps', undefined, []);
-        this.currentIpsMax = module.cwrap('currentIpsMax', undefined, []);
-        this.getTimesliceSizeUsec = module.cwrap('getTimesliceSizeUsec', 'number', []);
-        this.getTimestampUsec = module.cwrap('getTimestampUsec', 'number', []);
-        this.penDown = module.cwrap('penDown', undefined, ['number', 'number']);
-        this.penUp = module.cwrap('penUp', undefined, []);
-        this.keyDown = module.cwrap('keyDown', undefined, ['number']);
-        this.keyUp = module.cwrap('keyUp', undefined, ['number']);
+            this.cycle = module.cwrap('cycle', undefined, ['number']);
+            this.getFrame = module.cwrap('getFrame', 'number', []);
+            this.resetFrame = module.cwrap('resetFrame', undefined, []);
+            this.currentIps = module.cwrap('currentIps', undefined, []);
+            this.currentIpsMax = module.cwrap('currentIpsMax', undefined, []);
+            this.getTimesliceSizeUsec = module.cwrap('getTimesliceSizeUsec', 'number', []);
+            this.getTimestampUsec = module.cwrap('getTimestampUsec', 'number', []);
+            this.penDown = module.cwrap('penDown', undefined, ['number', 'number']);
+            this.penUp = module.cwrap('penUp', undefined, []);
+            this.keyDown = module.cwrap('keyDown', undefined, ['number']);
+            this.keyUp = module.cwrap('keyUp', undefined, ['number']);
 
-        this.amIDead = false;
-    }
+            this.amIDead = false;
+        }
 
-    static async create(nor, nand, sd, env) {
-        const { log } = env;
-        let module;
+        static async create(nor, nand, sd, env) {
+            const { log } = env;
+            let module;
 
-        module = await createModule({
-            noInitialRun: true,
-            print: log,
-            printErr: log,
-            locateFile: () => '../src/uarm_web.wasm',
-        });
+            module = await createModule({
+                noInitialRun: true,
+                print: log,
+                printErr: log,
+                locateFile: () => '../src/uarm_web.wasm',
+            });
 
-        const malloc = module.cwrap('malloc', 'number', ['number']);
+            const malloc = module.cwrap('malloc', 'number', ['number']);
 
-        let norPtr = malloc(nor.length);
-        let nandPtr = malloc(nand.length);
-        let sdPtr = sd ? malloc(sd.length) : 0;
+            let norPtr = malloc(nor.length);
+            let nandPtr = malloc(nand.length);
+            let sdPtr = sd ? malloc(sd.length) : 0;
 
-        module.HEAPU8.subarray(norPtr, norPtr + nor.length).set(nor);
-        module.HEAPU8.subarray(nandPtr, nandPtr + nand.length).set(nand);
-        if (sd) module.HEAPU8.subarray(sdPtr, sdPtr + sd.length).set(sd);
+            module.HEAPU8.subarray(norPtr, norPtr + nor.length).set(nor);
+            module.HEAPU8.subarray(nandPtr, nandPtr + nand.length).set(nand);
+            if (sd) module.HEAPU8.subarray(sdPtr, sdPtr + sd.length).set(sd);
 
-        module.callMain([]);
+            module.callMain([]);
 
-        module.ccall(
-            'webMain',
-            undefined,
-            ['number', 'number', 'number', 'number', 'number', 'number'],
-            [norPtr, nor.length, nandPtr, nand.length, sdPtr, sd ? sd.length : 0]
-        );
+            module.ccall(
+                'webMain',
+                undefined,
+                ['number', 'number', 'number', 'number', 'number', 'number'],
+                [norPtr, nor.length, nandPtr, nand.length, sdPtr, sd ? sd.length : 0]
+            );
 
-        return new Emulator(module, env);
-    }
+            return new Emulator(module, env);
+        }
 
-    stop() {
-        if (this.amIDead) return;
+        stop() {
+            if (this.amIDead) return;
 
-        if (this.immediateHandle) clearImmediate(this.immediateHandle);
-        if (this.timeoutHandle) clearTimeout(this.timeoutHandle);
+            if (this.immediateHandle) clearImmediate(this.immediateHandle);
+            if (this.timeoutHandle) clearTimeout(this.timeoutHandle);
 
-        this.timeoutHandle = this.immediateHandle = undefined;
-
-        this.log('emulator stopped');
-    }
-
-    start() {
-        if (this.amIDead) return;
-
-        if (this.timeoutHandle || this.immediateHandle) return;
-        this.lastSpeedUpdate = Number(this.getTimestampUsec());
-
-        const schedule = () => {
-            const now64 = this.getTimestampUsec();
-            const now = Number(now64);
-
-            try {
-                this.cycle(now64);
-            } catch (e) {
-                this.amIDead = true;
-                console.error(e);
-                this.log(e.message);
-
-                return;
-            }
-
-            this.render();
-
-            const timesliceRemainning = (this.getTimesliceSizeUsec() - Number(this.getTimestampUsec()) + now) / 1000;
             this.timeoutHandle = this.immediateHandle = undefined;
 
-            if (timesliceRemainning < 10) this.immediateHandle = setImmediate(schedule);
-            else this.timeoutHandle = setTimeout(schedule, timesliceRemainning);
+            this.log('emulator stopped');
+        }
 
-            if (now - this.lastSpeedUpdate > 1000000) {
-                this.updateSpeedDisplay();
-                this.lastSpeedUpdate = now;
+        start() {
+            if (this.amIDead) return;
+
+            if (this.timeoutHandle || this.immediateHandle) return;
+            this.lastSpeedUpdate = Number(this.getTimestampUsec());
+
+            const schedule = () => {
+                const now64 = this.getTimestampUsec();
+                const now = Number(now64);
+
+                try {
+                    this.cycle(now64);
+                } catch (e) {
+                    this.amIDead = true;
+                    console.error(e);
+                    this.log(e.message);
+
+                    return;
+                }
+
+                this.render();
+
+                const timesliceRemainning =
+                    (this.getTimesliceSizeUsec() - Number(this.getTimestampUsec()) + now) / 1000;
+                this.timeoutHandle = this.immediateHandle = undefined;
+
+                if (timesliceRemainning < 10) this.immediateHandle = setImmediate(schedule);
+                else this.timeoutHandle = setTimeout(schedule, timesliceRemainning);
+
+                if (now - this.lastSpeedUpdate > 1000000) {
+                    this.updateSpeedDisplay();
+                    this.lastSpeedUpdate = now;
+                }
+            };
+
+            this.log('emulator running');
+            schedule();
+        }
+
+        render() {
+            if (!this.module) return;
+
+            const framePtr = this.getFrame() >>> 2;
+            if (!framePtr) return;
+
+            const frame = this.module.HEAPU32.subarray(framePtr, framePtr + 320 * 320);
+
+            if (framePool.length === 0) {
+                this.onFrame(frame.slice().buffer);
+            } else {
+                const frameCopy = new Uint32Array(framePool.pop());
+                frameCopy.set(frame);
+
+                this.onFrame(frameCopy.buffer);
+            }
+
+            this.resetFrame();
+        }
+
+        updateSpeedDisplay() {
+            const currentIps = this.currentIps();
+            const currentIpsMax = this.currentIpsMax();
+
+            this.onSpeedDisplay(
+                `current ${(currentIps / 1e6).toFixed(2)} MIPS, limit ${(currentIpsMax / 1e6).toFixed(2)} MIPS -> ${(
+                    (currentIps / currentIpsMax) *
+                    100
+                ).toFixed(2)}%`
+            );
+        }
+    }
+
+    function mapButton(name) {
+        switch (name) {
+            case 'cal':
+                return 1;
+
+            case 'phone':
+                return 2;
+
+            case 'todo':
+                return 3;
+
+            case 'notes':
+                return 4;
+
+            case 'up':
+                return 5;
+
+            case 'down':
+                return 6;
+
+            default:
+                return -1;
+        }
+    }
+
+    function postReady() {
+        postMessage({ type: 'ready' });
+    }
+
+    function postFrame(data) {
+        postMessage({ type: 'frame', data }, [data]);
+    }
+
+    function postSpeed(text) {
+        postMessage({ type: 'speed', text });
+    }
+
+    function postLog(message) {
+        postMessage({ type: 'log', message });
+    }
+
+    function postError(reason) {
+        postMessage({ type: 'error', reason });
+    }
+
+    function postInitialized() {
+        postMessage({ type: 'initialized' });
+    }
+
+    async function handleMessage(message) {
+        let assertEmulator = (context) => {
+            if (!emulator) {
+                throw new Error(`${context}: emulator not running`);
             }
         };
 
-        this.log('emulator running');
-        schedule();
-    }
+        switch (message.type) {
+            case 'initialize':
+                emulator = await Emulator.create(message.nor, message.nand, message.sd, {
+                    onFrame: postFrame,
+                    onSpeedDisplay: postSpeed,
+                    log: postLog,
+                });
 
-    render() {
-        if (!this.module) return;
+                postInitialized();
 
-        const framePtr = this.getFrame() >>> 2;
-        if (!framePtr) return;
+                break;
 
-        const frame = this.module.HEAPU32.subarray(framePtr, framePtr + 320 * 320);
+            case 'start':
+                assertEmulator('start');
+                emulator.start();
+                break;
 
-        if (framePool.length === 0) {
-            this.onFrame(frame.slice().buffer);
-        } else {
-            const frameCopy = new Uint32Array(framePool.pop());
-            frameCopy.set(frame);
+            case 'stop':
+                assertEmulator('stop');
+                emulator.stop();
+                break;
 
-            this.onFrame(frameCopy.buffer);
-        }
+            case 'penDown':
+                assertEmulator('penDown');
+                emulator.penDown(message.x, message.y);
+                break;
 
-        this.resetFrame();
-    }
+            case 'penUp':
+                assertEmulator('penUp');
+                emulator.penUp();
+                break;
 
-    updateSpeedDisplay() {
-        const currentIps = this.currentIps();
-        const currentIpsMax = this.currentIpsMax();
+            case 'buttonDown': {
+                const button = mapButton(message.button);
+                if (button < 0) {
+                    console.error(`ignoring unknown button ${message.button}`);
+                    return;
+                }
 
-        this.onSpeedDisplay(
-            `current ${(currentIps / 1e6).toFixed(2)} MIPS, limit ${(currentIpsMax / 1e6).toFixed(2)} MIPS -> ${(
-                (currentIps / currentIpsMax) *
-                100
-            ).toFixed(2)}%`
-        );
-    }
-}
+                assertEmulator('buttonDown');
 
-function mapButton(name) {
-    switch (name) {
-        case 'cal':
-            return 1;
-
-        case 'phone':
-            return 2;
-
-        case 'todo':
-            return 3;
-
-        case 'notes':
-            return 4;
-
-        case 'up':
-            return 5;
-
-        case 'down':
-            return 6;
-
-        default:
-            return -1;
-    }
-}
-
-function postReady() {
-    postMessage({ type: 'ready' });
-}
-
-function postFrame(data) {
-    postMessage({ type: 'frame', data }, [data]);
-}
-
-function postSpeed(text) {
-    postMessage({ type: 'speed', text });
-}
-
-function postLog(message) {
-    postMessage({ type: 'log', message });
-}
-
-function postError(reason) {
-    postMessage({ type: 'error', reason });
-}
-
-function postInitialized() {
-    postMessage({ type: 'initialized' });
-}
-
-async function handleMessage(message) {
-    let assertEmulator = (context) => {
-        if (!emulator) {
-            throw new Error(`${context}: emulator not running`);
-        }
-    };
-
-    switch (message.type) {
-        case 'initialize':
-            emulator = await Emulator.create(message.nor, message.nand, message.sd, {
-                onFrame: postFrame,
-                onSpeedDisplay: postSpeed,
-                log: postLog,
-            });
-
-            postInitialized();
-
-            break;
-
-        case 'start':
-            assertEmulator('start');
-            emulator.start();
-            break;
-
-        case 'stop':
-            assertEmulator('stop');
-            emulator.stop();
-            break;
-
-        case 'penDown':
-            assertEmulator('penDown');
-            emulator.penDown(message.x, message.y);
-            break;
-
-        case 'penUp':
-            assertEmulator('penUp');
-            emulator.penUp();
-            break;
-
-        case 'buttonDown': {
-            const button = mapButton(message.button);
-            if (button < 0) {
-                console.error(`ignoring unknown button ${message.button}`);
-                return;
+                emulator.keyDown(button);
+                break;
             }
 
-            assertEmulator('buttonDown');
+            case 'buttonUp': {
+                const button = mapButton(message.button);
+                if (button < 0) {
+                    console.error(`ignoring unknown button ${message.button}`);
+                    return;
+                }
 
-            emulator.keyDown(button);
-            break;
-        }
+                assertEmulator('buttonUp');
 
-        case 'buttonUp': {
-            const button = mapButton(message.button);
-            if (button < 0) {
-                console.error(`ignoring unknown button ${message.button}`);
-                return;
+                emulator.keyUp(button);
+                break;
             }
 
-            assertEmulator('buttonUp');
+            case 'returnFrame':
+                framePool.push(message.frame);
+                break;
 
-            emulator.keyUp(button);
-            break;
-        }
-
-        case 'returnFrame':
-            framePool.push(message.frame);
-            break;
-
-        default:
-            console.error('unknown message from main thread', message);
-    }
-}
-
-async function dispatchMessages() {
-    if (dispatchInProgress || messageQueue.length === 0) return;
-    dispatchInProgress = true;
-
-    while (messageQueue.length > 0) {
-        try {
-            await handleMessage(messageQueue.shift());
-        } catch (e) {
-            postError(e);
+            default:
+                console.error('unknown message from main thread', message);
         }
     }
 
-    dispatchInProgress = false;
-}
+    async function dispatchMessages() {
+        if (dispatchInProgress || messageQueue.length === 0) return;
+        dispatchInProgress = true;
 
-async function main() {
-    postReady();
+        while (messageQueue.length > 0) {
+            try {
+                await handleMessage(messageQueue.shift());
+            } catch (e) {
+                postError(e);
+            }
+        }
 
-    onmessage = (e) => {
-        messageQueue.push(e.data);
-        dispatchMessages();
-    };
-}
+        dispatchInProgress = false;
+    }
 
-main().catch((e) => console.error(e));
+    async function main() {
+        postReady();
+
+        onmessage = (e) => {
+            messageQueue.push(e.data);
+            dispatchMessages();
+        };
+    }
+
+    main().catch((e) => console.error(e));
+})();
