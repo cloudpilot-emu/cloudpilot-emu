87e4866f1eb0ee2f5458dfd95b09f7a983f0196d
Christian Speckner <christian.speckner@mayflower.de>
2024-01-30 19:55:49 +0100

    Expose pacific on CLI.

--- src/cloudpilot/native/util.h
+++ src/cloudpilot/native/util.h
@@ -7,12 +7,13 @@
 
 namespace util {
     constexpr const char* SUPPORTED_DEVICES[] = {
-        "PalmPilot", "Pilot",           "PalmIIIc", "PalmV",           "PalmIII",   "PalmIIIe",
-        "PalmIIIx",  "PalmIIIxe",       "PalmVx",   "PalmVII",         "PalmVIIEZ", "PalmVIIx",
-        "PalmM500",  "PalmM505",        "PalmM515", "PalmM105",        "PalmM100",  "PalmM125",
-        "PalmM130",  "PalmI705",        "PalmI710", "HandEra330",      "PEG-S300",  "PEG-S320",
-        "PEG-T400",  "PEG-N600C/N610C", "PEG-T600", "PEG-N700C/N710C", "YSX1230",   "NR70",
-        "PEG-S500C", "HandEra330c",     "PalmM520", "Acer-S65",        "Acer-S11",  "Legend-P168"};
+        "PalmPilot",  "Pilot",           "PalmIIIc", "PalmV",           "PalmIII",   "PalmIIIe",
+        "PalmIIIx",   "PalmIIIxe",       "PalmVx",   "PalmVII",         "PalmVIIEZ", "PalmVIIx",
+        "PalmM500",   "PalmM505",        "PalmM515", "PalmM105",        "PalmM100",  "PalmM125",
+        "PalmM130",   "PalmI705",        "PalmI710", "HandEra330",      "PEG-S300",  "PEG-S320",
+        "PEG-T400",   "PEG-N600C/N610C", "PEG-T600", "PEG-N700C/N710C", "YSX1230",   "NR70",
+        "PEG-S500C",  "HandEra330c",     "PalmM520", "Acer-S65",        "Acer-S11",  "Legend-P168",
+        "PalmPacifiC"};
 
     bool readFile(string file, unique_ptr<uint8[]>& buffer, size_t& len);
 
