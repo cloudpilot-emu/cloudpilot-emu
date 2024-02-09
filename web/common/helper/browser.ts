0fdcd69c4baeb3b98e5708d62bb62835ac490528
Christian Speckner <christian.speckner@mayflower.de>
2024-02-09 22:12:00 +0100

    Detection for iOS 17.4

--- web/common/helper/browser.ts
+++ web/common/helper/browser.ts
@@ -3,6 +3,7 @@ export const isIOSSafari = !navigator.userAgent.match(/(crios)|(fxios)/i);
 export const isIOS =
     !!navigator.platform.match(/iPhone|iPad|iPod/) ||
     (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1);
+export const isIOS_174 = !!navigator.userAgent.match(/ 17_4 /);
 
 export const isAndroid = !!navigator.userAgent.match(/android/i);
 
