e345c46e91eabe7ceddca24ddb46859584f80c07
Christian Speckner <christian.speckner@mayflower.de>
2024-09-11 22:58:23 +0200

    Lock auto off timer to zero.

--- src/uarm/src/uarm/patches.c
+++ src/uarm/src/uarm/patches.c
@@ -12,7 +12,14 @@ static void tailpatch_UIInitialize(void* ctx, uint32_t syscall,
     syscall_SysSetAutoOffTime(sd, 0);
 }
 
+static void headpatch_SysSetAutoOffTime(void* ctx, uint32_t syscall, uint32_t* registers) {
+    registers[0] = 0;
+}
+
 void registerPatches(struct PatchDispatch* patchDispatch, struct SyscallDispatch* syscallDispatch) {
     patchDispatchAddPatch(patchDispatch, SYSCALL_UI_INITIALIZE, NULL, tailpatch_UIInitialize,
                           syscallDispatch);
+
+    patchDispatchAddPatch(patchDispatch, SYSCALL_SYS_SET_AUTO_OFF_TIME, headpatch_SysSetAutoOffTime,
+                          NULL, NULL);
 }
