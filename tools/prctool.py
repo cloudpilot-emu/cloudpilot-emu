bdde98282f7c984630975d5ffe0fd90484e38546
Christian Speckner <christian.speckner@mayflower.de>
2025-01-02 19:59:33 +0100

    Refactoring, support gap, appinfo and sortinfo.

--- tools/prctool.py
+++ tools/prctool.py
@@ -207,20 +207,8 @@ class RecordList(Struct):
 
         return RecordList(nextRecordListId, numRecords, offset)
 
-    @classmethod
-    def staticSize(self) -> int:
-        raise UnknownSizeException()
-
-    def size(self) -> int:
-        return 6 if self.numRecords > 0 else 8
-
     def serialize(self) -> bytes:
-        serialized = pack(self._format, self.nextRecordListId, self.numRecords)
-
-        if self.numRecords == 0:
-            serialized = serialized + b'\0\0'
-
-        return serialized
+        return pack(self._format, self.nextRecordListId, self.numRecords)
 
 
 class Entry(Struct):
@@ -280,12 +268,15 @@ class RecordEntry(Entry):
         return pack(self._format, self.localChunkId, self.attributes, encodedId)
 
 
-class Item:
+class Item[T: Entry]:
     id: int
     data: bytes
 
+    def entry(self, localChunkId: int) -> T:
+        raise NotImplementedError()
 
-class Resource(Item):
+
+class Resource(Item[ResourceEntry]):
     type: bytes
 
     def __init__(self, type: bytes, id: int, data: bytes):
@@ -293,8 +284,11 @@ class Resource(Item):
         self.id = id
         self.data = data
 
+    def entry(self, localChunkId: int) -> ResourceEntry:
+        return ResourceEntry(self.type, self.id, localChunkId)
 
-class Record(Item):
+
+class Record(Item[RecordEntry]):
     attributes: int
 
     def __init__(self, attributes: int, uniqueId: int, data: bytes):
@@ -302,8 +296,11 @@ class Record(Item):
         self.id = uniqueId
         self.data = data
 
+    def entry(self, localChunkId) -> RecordEntry:
+        return RecordEntry(self.attributes, self.id, localChunkId)
 
-class Database[T: Item]:
+
+class Database[T: Item[Entry]]:
     name: str
     attributes: int
     version: int
@@ -322,26 +319,101 @@ class Database[T: Item]:
 
     items: list[T]
 
-    def __init__(self, header: Header):
-        self.name = header.name
-        self.attributes = header.attributes
-        self.version = header.version
-        self.creationDate = header.creationDate
-        self.lastBackupDate = header.lastBackupDate
-        self.modificationDate = header.modificationDate
-        self.type = header.type
-        self.creator = header.creator
-        self.uniqueIdSeed = header.uniqueIdSeed
-        self.modificationNumber = header.modificationNumber
-
-        self.gapSize = 2
-        self.appInfo = None
-        self.sortInfo = None
+    def __init__(self,
+                 name: str,
+                 attributes: int,
+                 creationDate: int,
+                 modificationDate: int,
+                 lastBackupDate: int,
+                 type: bytes,
+                 creator: bytes,
+                 version: int = 0,
+                 modificationNumber: int = 0,
+                 uniqueIdSeed: int = 0,
+                 gapSize: int = 2,
+                 appInfo: bytes | None = None,
+                 sortInfo: bytes | None = None,
+                 ):
+        self.name = name
+        self.attributes = attributes
+        self.creationDate = creationDate
+        self.modificationDate = modificationDate
+        self.lastBackupDate = lastBackupDate
+        self.type = type
+        self.creator = creator
+        self.version = version
+        self.modificationNumber = modificationNumber
+        self.uniqueIdSeed = uniqueIdSeed
+        self.gapSize = gapSize
+        self.appInfo = appInfo
+        self.sortInfo = sortInfo
 
         self.items = []
 
+    @classmethod
+    def fromHeader(cls, header: Header) -> 'Database[T]':
+        return Database(
+            name=header.name,
+            attributes=header.attributes,
+            version=header.version,
+            creationDate=header.creationDate,
+            lastBackupDate=header.lastBackupDate,
+            modificationDate=header.modificationDate,
+            type=header.type,
+            creator=header.creator,
+            uniqueIdSeed=header.uniqueIdSeed,
+            modificationNumber=header.modificationNumber,
+        )
+
     def add(self, item: T):
         self.items.append(item)
+
+    def serialize(self) -> bytes:
+        offset = Header.staticSize() + RecordList.staticSize() + self.gapSize
+
+        appInfoId = 0
+        if self.appInfo is not None:
+            appInfoId = offset
+            offset += len(self.appInfo)
+
+        sortInfoId = 0
+        if self.sortInfo is not None:
+            sortInfoId = offset
+            offset += len(self.sortInfo)
+
+        header = Header(
+            name=self.name[:31],
+            attributes=self.attributes,
+            version=self.version,
+            creationDate=self.creationDate,
+            modificationDate=self.modificationDate,
+            lastBackupDate=self.lastBackupDate,
+            modificationNumber=self.modificationNumber,
+            appInfoId=appInfoId,
+            sortInfoId=sortInfoId,
+            type=options.type,
+            creator=options.creator,
+            uniqueIdSeed=self.uniqueIdSeed
+        )
+
+        recordList = RecordList(0, len(self.items))
+        gap = bytes([0 for _ in range(0, self.gapSize)])
+
+        serialized = header.serialize() + recordList.serialize() + gap
+
+        if self.appInfo is not None:
+            serialized += self.appInfo
+
+        if self.sortInfo is not None:
+            serialized += self.sortInfo
+
+        for item in self.items:
+            entry = item.entry(offset)
+
+            offset += entry.size()
+            serialized += entry.serialize()
+
+        return serialized
 
     def isResourceDb(self) -> bool:
         return self.attributes & dmHdrAttrResDB != 0
@@ -416,7 +488,7 @@ def parseDb(data: bytes) -> Database:
     db: Database[Item]
 
     if header.isResourceDb():
-        resourceDb = Database[Resource](header)
+        resourceDb = Database[Resource].fromHeader(header)
 
         appendRecords(resourceDb, recordList, offset, watermark,
                       parse=lambda data, offset: ResourceEntry.parse(
@@ -426,7 +498,7 @@ def parseDb(data: bytes) -> Database:
         db = cast(Database[Item], resourceDb)
 
     else:
-        recordDb = Database[Record](header)
+        recordDb = Database[Record].fromHeader(header)
 
         appendRecords(recordDb, recordList, offset, watermark,
                       parse=lambda data, offset: RecordEntry.parse(
@@ -450,21 +522,13 @@ def parseDb(data: bytes) -> Database:
 
 def splitResources(options: Any):
     try:
-        with open(options.target, "rb") as reader:
-            prcfile: bytes = reader.read()
-
-    except Exception:
-        print(f'unable to read {options.target}')
-        return
-
-    try:
-        db = parseDb(prcfile)
+        db = parseDb(options.database)
     except Exception as ex:
         print(f'failed to parse {options.target}: {ex}')
         return
 
     if not db.isResourceDb():
-        print(f'{options.target} is not a resource database')
+        print('not a resource database!')
         return
 
     resources = cast(Database[Resource], db).items
@@ -484,13 +548,7 @@ def splitResources(options: Any):
 
 
 def fixPdb(options: Any):
-    try:
-        with open(options.source, "rb") as reader:
-            pdbfile_in = reader.read()
-
-    except Exception:
-        print(f'unable to read {options.source}')
-        return
+    pdbfile_in: bytes = options.source
 
     pdbfile_out = bytearray(len(pdbfile_in))
     pdbfile_out[:] = pdbfile_in
@@ -543,84 +601,92 @@ def fixPdb(options: Any):
             writer.write(pdbfile_out)
 
     except Exception as ex:
-        print(f"could not write {options.destination}: {ex}")
+        print(f'could not write "{options.destination}"')
         return
 
-    print(f"wrote fixed database to {options.destination}")
+    print(f'wrote fixed database to "{options.destination}"')
 
 
 def dbInfo(options: Any):
     try:
-        with open(options.source, "rb") as reader:
-            data = reader.read()
-
-    except Exception:
-        print(f'unable to read {options.source}')
-        return
-
-    try:
-        db = parseDb(data)
-
-        print(f"name: {db.name}")
-        print(
-            f"kind: {'resource' if db.isResourceDb() else 'record'}")
-        print(f'type: {formatTag(db.type)}')
-        print(f'creator: {formatTag(db.creator)}')
-        print(f'created: {formatDate(db.creationDate)}')
-        print(f'modified: {formatDate(db.modificationDate)}')
-        print(f'last backup: {formatDate(db.lastBackupDate)}')
-        print(f'attributes: {formatDbAttribtes(db.attributes)}')
-        print(f'modification number: {db.modificationNumber}')
-        print(f'seed: {db.uniqueIdSeed}')
-        print("no appinfo" if db.appInfo ==
-              None else f'appinfo: {len(cast(bytes, db.appInfo))} bytes')
-        print("no sortinfo" if db.sortInfo ==
-              None else f'sortinfo: {len(cast(bytes, db.sortInfo))} bytes')
-        print(f'gap: {db.gapSize} bytes')
-        print()
-
-        if db.isResourceDb():
-            print("no resources" if len(db.items) ==
-                  0 else f'{len(db.items)} resources')
-        else:
-            print("no records" if len(db.items) ==
-                  0 else f'{len(db.items)} records')
+        db = parseDb(options.source)
 
     except Exception as ex:
         print(f'failed to parse database: {ex}')
-        print(traceback.format_exc())
+        return
+
+    print(f"name: {db.name}")
+    print(
+        f"kind: {'resource' if db.isResourceDb() else 'record'}")
+    print(f'type: {formatTag(db.type)}')
+    print(f'creator: {formatTag(db.creator)}')
+    print(f'created: {formatDate(db.creationDate)}')
+    print(f'modified: {formatDate(db.modificationDate)}')
+    print(f'last backup: {formatDate(db.lastBackupDate)}')
+    print(f'attributes: {formatDbAttribtes(db.attributes)}')
+    print(f'modification number: {db.modificationNumber}')
+    print(f'seed: {db.uniqueIdSeed}')
+    print("no appinfo" if db.appInfo is None else f'appinfo: {
+        len(db.appInfo)} bytes')
+    print("no sortinfo" if db.sortInfo is None else f'sortinfo: {
+        len(db.sortInfo)} bytes')
+    print(f'gap: {db.gapSize} bytes')
+    print()
+
+    if db.isResourceDb():
+        print("no resources" if len(db.items) ==
+              0 else f'{len(db.items)} resources')
+    else:
+        print("no records" if len(db.items) ==
+              0 else f'{len(db.items)} records')
+
+    if options.appinfo is not None and db.appInfo is not None:
+        try:
+            with open(options.appinfo, 'bw') as writer:
+                writer.write(db.appInfo)
+
+            print(f'wrote appinfo block to "{options.appinfo}"')
+
+        except Exception as ex:
+            print(f'could not write "{options.appinfo}"')
+            return
+
+    if options.sortinfo is not None and db.sortInfo is not None:
+        try:
+            with open(options.sortinfo, 'bw') as writer:
+                writer.write(db.sortInfo)
+
+            print(f'wrote sortinfo block to "{options.sortinfo}"')
+
+        except Exception as ex:
+            print(f'could not write "{options.sortinfo}"')
+            return
 
 
 def createDb(options: Any):
-    attributes = 0 if options.attributes == None else options.attributes
+    attributes = 0 if options.attributes is None else options.attributes
     if options.resource:
         attributes |= dmHdrAttrResDB
 
     now = int((datetime.datetime.now() -
                datetime.datetime(year=1904, month=1, day=1)).total_seconds())
 
-    header = Header(
-        name=options.name[:31],
+    db = Database[Item[Entry]](
+        name=options.name,
         attributes=attributes,
-        version=0,
         creationDate=now,
         modificationDate=now,
         lastBackupDate=now,
-        modificationNumber=0,
-        appInfoId=0,
-        sortInfoId=0,
         type=options.type,
         creator=options.creator,
-        uniqueIdSeed=0
+        gapSize=options.gap if options.gap > 0 else 0,
+        appInfo=options.appinfo,
+        sortInfo=options.sortinfo
     )
-
-    recordList = RecordList(0, 0)
-
-    serializedDb = header.serialize() + recordList.serialize()
 
     try:
         with open(options.destination, 'bw') as writer:
-            writer.write(serializedDb)
+            writer.write(db.serialize())
 
     except Exception as ex:
         print(f"could not write {options.destination}: {ex}")
@@ -649,7 +715,7 @@ def typeAttributes(list: str):
     def convert(name: str) -> int:
         value = attributeValues.get(name)
 
-        if value == None or name == "res":
+        if value is None or name == "res":
             raise argparse.ArgumentTypeError(msg)
 
         return cast(int, value)
@@ -661,28 +727,40 @@ def typeAttributes(list: str):
     return value
 
 
+def typeFile(name: str):
+    try:
+        with open(name, "rb") as reader:
+            return reader.read()
+
+    except Exception:
+        raise argparse.ArgumentTypeError(f'unable to read file "{name}"')
+
+
 parser = argparse.ArgumentParser(description="Tools for handling prc files")
 subparsers = parser.add_subparsers(help="subcommand", dest="subcommand")
 
 parserSplit = subparsers.add_parser(
     "split-resources", help="split into individual resources", description="Split the .prc into its individual resources")
-parserSplit.add_argument("target", metavar="<target>",
-                         type=str, help="target file")
+parserSplit.add_argument("database", metavar="<database file>",
+                         type=typeFile, help="source database")
 parserSplit.add_argument("prefix", metavar="<prefix>",
-                         type=str, help="target file")
+                         type=str, help="prefix for created resource files")
 
 parserFixPdb = subparsers.add_parser("fix-pdb", help="fix .pdb files",
                                      description="Fix record databases that were exported by CloudpilotEmu < 1.8.9 with PalmOS < 3 and that contain bad deleted records")
 parserFixPdb.add_argument("source", metavar="<source>",
-                          type=str, help="source file")
+                          type=typeFile, help="source file")
 parserFixPdb.add_argument("destination", metavar="<destination>",
                           type=str, help="destination file")
 
 parserDbInfo = subparsers.add_parser(
     'info', help="show DB info", description="show info and statistics on database")
-
 parserDbInfo.add_argument("source", metavar="<source>",
-                          type=str, help="source file")
+                          type=typeFile, help="source file")
+parserDbInfo.add_argument("--appinfo", metavar="<file>",
+                          type=str, help="write appinfo block to <file>")
+parserDbInfo.add_argument("--sortinfo", metavar="<file>",
+                          type=str, help="write sortinfo block to <file>")
 
 parserCreateDb = subparsers.add_parser(
     "create", help="create new empty DB", description="create a new empty database")
@@ -700,6 +778,11 @@ parserCreateDb.add_argument("--creator", type=typeTag, metavar="creator",
                             required=True, help="DB creator (exactly 4 ASCII chars)")
 parserCreateDb.add_argument(
     "--attributes", type=typeAttributes, help="DB attributes")
+parserCreateDb.add_argument("--gap", type=int, default=2, help="gap size")
+parserCreateDb.add_argument(
+    "--appinfo", metavar="file", type=typeFile, help="appinfo block")
+parserCreateDb.add_argument(
+    "--sortinfo", metavar="file", type=typeFile, help="sortinfo block")
 
 options = parser.parse_args()
 
