db078f4dbe299167ecf7fb6269d60b6ca650639a
Christian Speckner <christian.speckner@mayflower.de>
2026-08-14 10:00:46 +0200

    Start bootstrapping PV.

---
+++ src/uarm/uarm/soc_pv.cpp
@@ -0,0 +1,117 @@
+// clang-format off
+#include "soc_generic_impl.h" // IWYU pragma: keep
+// clang-format on
+
+#include "soc_pv.h"
+
+#include <cstdint>
+
+#include "CPU.h"
+#include "RAM.h"
+#include "ROM.h"
+#include "device_type5.h"
+#include "memory_buffer.h"
+#include "patch68k.h"
+#include "patch_dispatch.h"
+#include "patches.h"
+#include "peephole.h"
+#include "syscall_dispatch.h"
+
+#define CPUID_PXA255 0x69052D06ul
+
+#define ROM_BASE 0x10000000
+#define RAM_BASE 0x20000000
+
+#define PV_TINYRAM_BASE 0xffff0000
+#define PV_TINYRAM_SIZE 0x00000100
+
+namespace {
+    uint32_t sanitizeRamSize(uint32_t ramSize) {
+        uint32_t sanitizedSize = (ramSize >> 20) << 20;
+
+        if (ramSize & ((1 << 20) - 1)) sanitizedSize += (1 << 20);
+
+        return sanitizedSize;
+    }
+}  // namespace
+
+SocPV::SocPV(uint32_t ramSize, void *romData, const uint32_t romSize) {
+    ramSize = sanitizeRamSize((ramSize));
+    this->ramSize = ramSize;
+
+    AllocateBuffers();
+
+    cpu = cpuInit(ROM_BASE, mem, ARM_MEMORY_SYSTEM_MPU, true, false, -1, CPUID_PXA255, 0x0B16A16AUL,
+                  patchDispatch, pacePatch, systemState);
+
+    patchDispatchSetCpu(patchDispatch, cpu);
+
+    syscallDispatch = initSyscallDispatch(this);
+    patchContext = registerPatches(patchDispatch, syscallDispatch, cpu, systemState);
+
+    ram = ramInit(mem, this, RAM_BASE, ramSize, &bufferMemory, true);
+    tinyRam = ramInit(mem, this, PV_TINYRAM_BASE, PV_TINYRAM_SIZE, &bufferTinyRam, false);
+    rom = romInit(mem, ROM_BASE, romData, romSize);
+
+    void *peepholeBuffer = romGetPeepholeBuffer(rom);
+
+    pacePatchInit(pacePatch, ROM_BASE, peepholeBuffer, romSize);
+    peepholeOptimize((uint32_t *)peepholeBuffer, romSize);
+    patch68kInit(PATCH_68K_NVFS);
+
+    powerOnState->Save(*this);
+    SdEject();
+}
+
+uint32_t *SocPV::GetPendingFrame() { return nullptr; }
+
+void SocPV::ResetPendingFrame() {}
+
+enum DeviceType5 SocPV::GetDeviceType() { return deviceTypePV; }
+
+void SocPV::SuspendTimerInterrupts(bool suspendInterrupts) {}
+
+bool SocPV::LcdEnabled() { return true; }
+
+uint32_t SocPV::DispatchTicks(uint32_t clientType, uint32_t batchedTicks) { return 1; }
+
+void SocPV::OnSetFramebufferDirty() {}
+
+void SocPV::OnSleep() {}
+
+void SocPV::OnWakeup() {}
+
+void SocPV::OnSetAudioQueue(struct AudioQueue *audioQueue) {}
+
+void SocPV::OnSetPcmOutputEnabled() {}
+
+void SocPV::OnSetPcmSuspended() {}
+
+void SocPV::OnSdInsert() {}
+
+void SocPV::OnSdEject() {}
+
+void SocPV::OnTouch(int x, int y) {}
+
+void SocPV::OnEngageKey(KeyId key, bool down) {}
+
+void SocPV::OnReset() {}
+
+void SocPV::OnLoad(SavestateLoader<ChunkType> &loader) {}
+
+template <typename T>
+void SocPV::OnSave(T &savestate) {}
+
+void SocPV::AllocateBuffers() {
+    size_t memoryBufferSize = ramSize + MEMORY_BUFFER_GRANULARITY;
+
+    bool success = memoryBufferAllocate(&bufferMemory, memoryBufferSize);
+    success = success && memoryBufferGetSubBuffer(&bufferMemory, &bufferTinyRam, ramSize,
+                                                  MEMORY_BUFFER_GRANULARITY);
+
+    if (!success) ERR("failed to allocate memory buffer");
+}
+
+template void SocGeneric<SocPV>::Save<Savestate<ChunkType>>(Savestate<ChunkType> &savestate);
+template void SocGeneric<SocPV>::Save<SavestateProbe<ChunkType>>(
+    SavestateProbe<ChunkType> &savestate);
---
+++ src/uarm/uarm/soc_pv.h
@@ -0,0 +1,52 @@
+#ifndef _SOC_PV_H_
+#define _SOC_PV_H_
+
+#include "RAM.h"
+#include "memory_buffer.h"
+#include "soc_generic.h"
+
+class SocPV : public SocGeneric<SocPV> {
+    friend SocGeneric<SocPV>;
+
+   public:
+    static constexpr int MEMORY_SYSTEM_KIND = ARM_MEMORY_SYSTEM_MPU;
+
+   public:
+    SocPV(uint32_t ramSize, void *romData, const uint32_t romSize);
+
+    uint32_t *GetPendingFrame() override;
+    void ResetPendingFrame() override;
+    enum DeviceType5 GetDeviceType() override;
+    void SuspendTimerInterrupts(bool suspendInterrupts) override;
+    bool LcdEnabled() override;
+
+    uint32_t DispatchTicks(uint32_t clientType, uint32_t batchedTicks);
+
+   protected:
+    void OnSetFramebufferDirty() override;
+    void OnSleep() override;
+    void OnWakeup() override;
+    void OnSetAudioQueue(struct AudioQueue *audioQueue) override;
+    void OnSetPcmOutputEnabled() override;
+    void OnSetPcmSuspended() override;
+    void OnSdInsert() override;
+    void OnSdEject() override;
+
+    void OnTouch(int x, int y);
+    void OnEngageKey(KeyId key, bool down);
+    void OnReset();
+
+    void OnLoad(SavestateLoader<ChunkType> &loader);
+
+    template <typename T>
+    void OnSave(T &savestate);
+
+   private:
+    void AllocateBuffers();
+
+   private:
+    MemoryBuffer bufferTinyRam;
+    ArmRam *tinyRam;
+};
+
+#endif  // _SOC_PV_H_
